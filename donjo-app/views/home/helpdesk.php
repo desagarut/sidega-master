d7a064fb2a92361a21219b92e96b26c588125a24
Desa Garut <desagarut.net@gmail.com>
2022-03-17 13:05:44 +0700

    Update Helpdesk
    Update url helpdesk

--- donjo-app/views/home/helpdesk.php
+++ donjo-app/views/home/helpdesk.php
@@ -11,7 +11,7 @@
 					</div>
 					<div class="box-body text-center">
                         <a href="<?=site_url('webmail')?>" target="_blank"><img src="<?= base_url("assets/files/logo/email.fw.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt="" ></a>
-                        <a href="https://help.desagarut.id/staff/login" target="_blank"><img src="<?= base_url("assets/files/logo/helpdesk.fw.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
+                        <a href="https://help.desagarut.id" target="_blank"><img src="<?= base_url("assets/files/logo/helpdesk.fw.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
                         <a href="https://chat.whatsapp.com/IR2VtLpT2Fx0ujlNMm3nD9" target="_blank"><img src="<?= base_url("assets/files/logo/whatsapp.fw.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt="Whatsapp Komunitas Desa Garut"></a>
                         <div class="box-footer text-center">
                         <p><i>Gunakan email & password untuk login ke halaman Email & Helpdesk Desa Garut<br/>
