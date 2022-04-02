76c953dd6280fc8f70c38c789ba29c26ebea47cb
Desa Garut <desagarut.net@gmail.com>
2022-04-02 16:30:56 +0700

    m
    m

--- themes/hijau/partials/tawa/index.php
+++ themes/hijau/partials/tawa/index.php
@@ -45,20 +45,9 @@
   </div>
 </div>
 <div class="row">
-  <div class="col-lg-3 col-md-6 align-items-stretch owl-carousel portfolio-details-carousel">
-    <div class="member" data-aos="fade-up">
-      <div class="member-img"> <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/transport.png" ) ?>" class="img-fluid" alt="" style="width:100%; height:225px"> </div>
-      <div class="member-info" style="background-color:#B7FFDB">
-        <h4> <strong style="color:#C00">TAWA</strong>: TRANSPORTASI WARGA </h4>
-        <p class="text-center"><strong>adalah </strong> wadah bagi masyarakat <strong>
-          <?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>
-          </strong>, yang bertujuan untuk fasilitasi seluruh aktivitas warga, baik warga desa atau pendatang yang ingin melakukan perjalanan, <br/>
-          selain itu melalui aplikasi ini dapat memberikan peluang bagi masyarakat untuk meningkatkan penghasilan keluarga serta sebagai salah satu upaya dalam mengurangi angka pengangguran di desa, dengan demikian diharapkan dapat membantu meningkatkan pertumbuhan ekonomi masyarakat <strong>
-          <?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>
-          </strong>.</p>
-      </div>
-    </div>
-  </div>
+<div class="col-md-3">
+</div>
+<div class="col-md-9">
   <?php if($main) : ?>
   <?php foreach($main as $data) : ?>
   <?php if(is_file(LOKASI_GALERI . "kecil_" . $data['gambar'])) : ?>
@@ -96,5 +85,6 @@
   <?php endif ?>
   <?php endforeach ?>
   <?php endif ?>
+</div>
 </div>
 <!-- End Our Team Section --> 
