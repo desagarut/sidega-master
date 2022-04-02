3d51b8e3fc37c312934882e038b3e0289a0ded50
Desa Garut <desagarut.net@gmail.com>
2022-04-02 16:46:19 +0700

    f
    f

--- themes/hijau/partials/tawa/index.php
+++ themes/hijau/partials/tawa/index.php
@@ -45,9 +45,8 @@
   </div>
 </div>
 <div class="row">
-<div class="col-md-3">
-  <div class="align-items-stretch owl-carousel portfolio-details-carousel">
-    <div class="member" data-aos="fade-up">
+<div class="col-md-3 align-items-stretch owl-carousel portfolio-details-carousel">
+     <div class="member" data-aos="fade-up">
       <div class="member-img"> <img src="<?= base_url("$this->theme_folder/$this->theme/assets/img/transport.png" ) ?>" class="img-fluid" alt="" style="width:100%; height:225px"> </div>
       <div class="member-info" style="background-color:#B7FFDB">
         <h4> <strong style="color:#C00">TAWA</strong>: TRANSPORTASI WARGA </h4>
@@ -58,7 +57,6 @@
           <?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>
           </strong>.</p>
       </div>
-    </div>
   </div>
 </div>
 <div class="col-md-9">
