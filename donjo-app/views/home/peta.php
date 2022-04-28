c6eacc21cc10742adb570c2c738f9d92dba7d73d
Desa Garut <68489248+desagarut@users.noreply.github.com>
2022-04-28 23:03:17 +0700

    Update Version 5.4
    Update Version 5.4

--- donjo-app/views/home/peta.php
+++ donjo-app/views/home/peta.php
@@ -1,5 +1,5 @@
 <?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
-<script async defer
+<script
 src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVUVWZSy8N0A9brJyoe_3UiwBzUB9j8vE&callback=initMap">
 </script>
 
