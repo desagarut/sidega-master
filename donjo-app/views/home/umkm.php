730e1e069c270d64cfe966ffaef23ce0c3fef0e1
Desa Garut <desagarut.net@gmail.com>
2022-05-31 20:57:05 +0700

    Update umkm.php

--- donjo-app/views/home/umkm.php
+++ donjo-app/views/home/umkm.php
@@ -1,7 +1,7 @@
             <div class='col-md-4'>
 				<div class="box box-primary box-solid">
 					<div class="box-header with-border">
-						<h3 class="box-title">UMKM</h3>
+						<h3 class="box-title">Potensi UMKM</h3>
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
                             </button>
