2de74a81b5062cd3d4c14ab6ae5487a0b49bbeec
Desa Garut <desagarut.net@gmail.com>
2022-06-01 11:27:09 +0700

    Update umkm.php

--- donjo-app/views/home/umkm.php
+++ donjo-app/views/home/umkm.php
@@ -1,6 +1,6 @@
             <div class='col-md-4'>
-				<div class="box box-primary box-solid">
-					<div class="box-header with-border">
+				<div class="box box-warning box-solid">
+					<div class="box-header bg-aqua with-border">
 						<h3 class="box-title">Potensi UMKM</h3>
                         <div class="box-tools pull-right">
                             <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -10,11 +10,18 @@
                         </div>
 					</div>
 					<div class="box-body text-center">
+                        <p>
+                            Data Potensi Usaha Kecil Menengah <?= ucwords($this->setting->sebutan_desa . " " . $desa['nama_desa']); ?>
+                        </p>
                         <a href="<?=site_url('toko_warga')?>"><img src="<?= base_url("assets/files/logo/toko.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
                         <a href="<?=site_url('tawa')?>"><img src="<?= base_url("assets/files/logo/transport.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
                         <a href="<?=site_url('tukang')?>"><img src="<?= base_url("assets/files/logo/tukang.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
                         <a href="<?=site_url('wisata')?>"><img src="<?= base_url("assets/files/logo/wisata.png" ) ?>" class="img-fluid responsive" width="70px" height="70px" alt=""></a>
                         <div class="box-footer text-center">
+                        <p>
+                            <strong style="color:blueviolet">"Tinggal Di Desa, Rezeki Kota, Bisnis Mendunia"<br/></strong>
+                            <a class="pull-right">~Ridwan Kamil~</a>
+                        </p>
                         </div>
                     </div>
                 </div>
