ec5d8c132a10a4f33efd322427dce671d40f7d36
Desa Garut <desagarut.net@gmail.com>
2022-06-23 00:45:17 +0700

    Update portal-atas.php

--- themes/hijau/partials/portal-atas.php
+++ themes/hijau/partials/portal-atas.php
@@ -1,46 +1,4 @@
-<!-- ======= Features Section ======= -->
 
-<div class="col-lg-4 col-md-12 col-12">
-  <div class="offer-content">
-    <div class="image"> <img src="#" alt="#"> <span class="sale-tag">-50%</span> </div>
-    <div class="text">
-      <h2><a href="product-grids.html">Bluetooth Headphone</a></h2>
-      <ul class="review">
-        <li><i class="lni lni-star-filled"></i></li>
-        <li><i class="lni lni-star-filled"></i></li>
-        <li><i class="lni lni-star-filled"></i></li>
-        <li><i class="lni lni-star-filled"></i></li>
-        <li><i class="lni lni-star-filled"></i></li>
-        <li><span>5.0 Review(s)</span></li>
-      </ul>
-      <div class="price"> <span>$200.00</span> <span class="discount-price">$400.00</span> </div>
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry incididunt ut
-        eiusmod tempor labores.</p>
-    </div>
-    <div class="box-head">
-      <div class="box">
-        <h1 id="days">000</h1>
-        <h2 id="daystxt">Days</h2>
-      </div>
-      <div class="box">
-        <h1 id="hours">00</h1>
-        <h2 id="hourstxt">Hours</h2>
-      </div>
-      <div class="box">
-        <h1 id="minutes">00</h1>
-        <h2 id="minutestxt">Minutes</h2>
-      </div>
-      <div class="box">
-        <h1 id="seconds">00</h1>
-        <h2 id="secondstxt">Secondes</h2>
-      </div>
-    </div>
-    <div style="background: rgb(204, 24, 24);" class="alert">
-      <h1 style="padding: 50px 80px;color: white;">We are sorry, Event ended ! </h1>
-    </div>
-  </div>
-</div>
-<!--
 <section id="features" class="features" style="padding-top:-60px;">
   <div class="row" data-aos="fade-left">
     <div class="col-lg-12 col-md-4 mt-0"> <a href="https://api.whatsapp.com/send?phone=+62<?= $desa['telepon']; ?>" target="_blank">
@@ -69,5 +27,5 @@
       </div>
     </div>
   </div>
-</section>-->
+</section>
 <!-- End Features Section --> 
