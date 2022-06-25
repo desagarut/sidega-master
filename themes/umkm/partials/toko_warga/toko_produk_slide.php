78ac4a6f2d13fcc7b02ae6a3a5129c4931785edb
Desa Garut <desagarut.net@gmail.com>
2022-06-25 22:29:37 +0700

    Update Toko
    Update Toko

--- themes/umkm/partials/toko_warga/toko_produk_slide.php
+++ themes/umkm/partials/toko_warga/toko_produk_slide.php
@@ -22,7 +22,7 @@
                   :</small>
                   <?= $rupiah($album['harga']) ?>
                   <a href="https://wa.me/+62<?= $sub['no_hp_toko'] ?>?text=Assalamu'alaikum%2C%20Saya%20ingin%20membeli%20produk%20<?= $album['nama'] ?>%20yang%20ditawarkan%20di%20website%20*<?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>*.%20Apakah%20<?= $album['nama'] ?>%20masih%20tersedia%3F%20<?= site_url('first/produk_show/'.$sub['id']) ?>" target="_blank" title="Beli Sekarang">
-                  <button class="btn btn-danger"><i class="icofont-whatsapp"></i> Beli Langsung</button>
+                  <button class="btn btn-danger"><i class="icofont-whatsapp"></i> Beli </button>
                   </a></h3>
                 <div class="button"> <a class="" href="<?= site_url('first/produk_show/'.$data['id']) ?>">
                   <button class="btn btn-warning"><i class="ri-store-2-fill" style="color:#fff;"></i> Produk:
--- themes/umkm/partials/toko_warga/toko_slide.php
+++ themes/umkm/partials/toko_warga/toko_slide.php
@@ -13,12 +13,12 @@
                                 style="background-image: url(<?= AmbilGaleri($data['gambar'],'sedang') ?>);">
               <div class="content">
                 <h2 style="color:#FFF; text-shadow: 5px 5px 5px #081828;
-    -webkit-text-stroke: 0.25px #081828;"><span></span> UMKM :
+    -webkit-text-stroke: 0.25px #081828;"><span></span> Nama Usaha :
                   <?= $data['nama'] ?>
                 </h2>
                 <div class="button"> <a class="" href="<?= site_url('first/produk_show/'.$data['id']) ?>">
-                  <button class="btn btn-warning"><i class="ri-store-2-fill" style="color:#fff;"></i> UMKM:
-                  <?= $data['nama'] ?>
+                  <button class="btn btn-warning"><i class="ri-store-2-fill" style="color:#fff;"></i> Pengelola:
+                  <?= $data['nama_pengelola'] ?>
                   </button>
                   </a> <a href="https://wa.me/+62<?= $data['no_hp_toko'] ?>?text=Assalamu'alaikum%2C%20Saya%20tertarik%20dengan%20produk%20yang%20ditawarkan%20di%20website%20*<?= ucfirst($this->setting->sebutan_desa).' '.ucwords($desa['nama_desa']) ?>*.%20Apakah%20<?= $data['nama'] ?>%20masih%20buka%3F%20<?= site_url('first/produk_show/'.$data['id']) ?>" target="_blank" title="pesan">
                   <button class="btn btn-success"><i class="icofont-whatsapp"></i> Hubungi</button>
