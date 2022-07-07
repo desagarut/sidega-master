5dd19e952491e8a049bcf41845472b9040e9a956
Desa Garut <desagarut.net@gmail.com>
2022-07-07 12:15:25 +0700

    Up Tema UMKM
    Arsip berita
    Menu Kategori

--- themes/umkm/layouts/arsip.tpl.php
+++ themes/umkm/layouts/arsip.tpl.php
@@ -1,36 +1,56 @@
-<?php  if(!defined('BASEPATH')) exit('No direct script access allowed'); ?>
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-<?php $this->load->view($folder_themes . '/commons/meta') ?>
-<?php $this->load->view($folder_themes . '/commons/for_css') ?>
-</head>
-<body>
-<?php $this->load->view($folder_themes .'/commons/header') ?>
-<?php // $this->load->view($folder_themes .'/partials/newsticker') ?>
-<section id="breadcrumbs" class="breadcrumbs">
-  <div class="container">
-    <div class="d-flex justify-content-between align-items-center">
-      <h5>Arsip Artikel</h5>
-      <ol>
-        <li><a href="<?= site_url("first"); ?>">Home</a></li>
-        <li>Arsip artikel</li>
-      </ol>
-    </div>
-  </div>
-</section>
-<!-- End Breadcrumbs -->
-<section id="blog" class="blog">
-  <div class="container">
-    <div class="row">
-        <?php $this->load->view($folder_themes .'/partials/arsip.php') ?>
-        <?php $this->load->view($folder_themes .'/partials/sidebar.php') ?>
-    </div>
-  </div>
-</section>
 
-<?php $this->load->view($folder_themes .'/commons/footer') ?>
-<?php $this->load->view($folder_themes . '/commons/for_js') ?>
+<head>
+  <?php $this->load->view($folder_themes . '/commons/meta') ?>
+  <?php $this->load->view($folder_themes . '/commons/for_css') ?>
+</head>
+
+<body>
+  <?php $this->load->view($folder_themes . '/commons/header') ?>
+  <?php // $this->load->view($folder_themes .'/partials/newsticker') 
+  ?>
+  <div class="breadcrumbs">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 col-md-6 col-12">
+          <div class="breadcrumbs-content">
+            <h1 class="page-title">Arsip Artikel</h1>
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-12">
+          <ul class="breadcrumb-nav">
+            <li><a href="<?= site_url("first"); ?>"><i class="lni lni-home"></i> Home</a></li>
+            <li>Arsip artikel</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Breadcrumbs -->
+
+
+  <!-- ======= Section ======= -->
+  <section class="item-details section">
+    <div class="container">
+      <div class="product-details-info">
+        <div class="row">
+          <div class="col-lg-8 col-sm-12 col-12">
+            <div class="row">
+              <?php $this->load->view($folder_themes . '/partials/arsip.php') ?>
+            </div>
+          </div>
+          <div class="col-lg-4 col-12">
+            <?php $this->load->view($folder_themes . '/partials/sidebar.php') ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php $this->load->view($folder_themes . '/commons/footer') ?>
+  <?php $this->load->view($folder_themes . '/commons/for_js') ?>
 </body>
+
 </html>
