6d8f96138ed6e9c37088592b3fcf2d1b58050399
Desa Garut <desagarut.net@gmail.com>
2022-08-27 07:42:33 +0700

    Update V.5.4.3
    Feed
    rss

--- donjo-app/libraries/Feed_Reader.php
+++ donjo-app/libraries/Feed_Reader.php
@@ -1,10 +1,9 @@
 <?php class Feed_Reader
 {
-	function Feed_Reader($sumber_feed='', $feed_sthg='')
+	function Feed_Reader($sumber_feed='')
 	{
 		include_once('FeedParser.php');
 		$this->sumber_feed = $sumber_feed;
-		$this->feed_sthg = $feed_sthg;
 	}
 
 }
