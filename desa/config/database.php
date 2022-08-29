48fa531518fd3017d98ba4b797047052abb755fa
Desa Garut <desagarut.net@gmail.com>
2022-08-29 10:32:47 +0700

    BACK TO SIDEGA MASTER 5.4
    BACK TO SIDEGA MASTER 5.4

--- desa/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-//$db['default']['database'] = 'kecamat1_3205362004_sukarame_sidega';
-$db['default']['database'] = 'sidega-master-5.4';
-$db['default']['stricton'] = TRUE;
-?>
