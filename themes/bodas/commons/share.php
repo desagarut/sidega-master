915e0a87fa112cca78a903dd6cb5a0ad962f145b
Desa Garut <desagarut.net@gmail.com>
2023-03-16 11:57:05 +0700

    Update Tema Bodas

--- themes/bodas/commons/share.php
+++ themes/bodas/commons/share.php
@@ -1,16 +1,8 @@
+<div class="d-flex pt-2 text-center">
+  <a class="btn btn-outline-info btn-social" href="" target="_blank"><i class="fab fa-twitter"></i></a>&nbsp;
+  <a class="btn btn-outline-primary btn-social" href="" target="_blank"><i class="fab fa-facebook-f"></i></a>&nbsp;
+  <a class="btn btn-outline-success btn-social" href="https://api.whatsapp.com/send?l=id&text=<?= site_url('artikel/' . buat_slug($article)) ?>" target="_blank"><i class="fab fa-whatsapp"></i></a>&nbsp;
+  <a class="btn btn-outline-primary btn-social" href="" target="_blank"><i class="fab fa-telegram"></i></a>&nbsp;
+  <a class="btn btn-outline-danger btn-social" href="" target="_blank"><i class="fab fa-instagram"></i></a>
 
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your share button code -->
-<div class="row"><h6>Bagikan artikel di: </h6>
-<div class="fb-share-button" data-href="<?= $link; ?>" data-layout="button_count"></div>
-<a href="https://api.whatsapp.com/send?text=<?= $link; ?>" target="_blank" rel="nofollow noopener" class="button btn-default"><i class="fab fa-whatsapp" style="color:green"></i>&nbsp;WhatsApp</a>
 </div>
--- themes/bodas/partials/artikel.php
+++ themes/bodas/partials/artikel.php
@@ -3,7 +3,7 @@
 <div class="row">
 	<div class="product-images">
 		<main id="gallery">
-			<div class="main-img text-center shadow" style="padding: 10px 10px 10px 10px;">
+			<div class="main-img text-center shadow wow fadeInLeft" style="padding: 10px 10px 10px 10px;">
 				<?php if ($article['gambar']) : ?>
 					<img class="img-fluid" src="<?= AmbilFotoArtikel($article['gambar' . $i], 'sedang') ?>" alt="<?= $article['judul'] ?>">
 				<?php else : ?>
@@ -11,22 +11,22 @@
 				<?php endif; ?>
 			</div>
 		</main>
-		<div class=" py-3 wow fadeInUp" data-wow-delay="0.3s">
+		<div class=" py-3 wow fadeInRight data-wow-delay="0.3s">
 			<h2 class="text-start"><?= $article['judul'] ?></h2>
 			<p>
 				<i class="fa fa-user-tie text-primary me-2"></i> <?= $article['owner'] ?> &nbsp;
 				<i class="fa fa-clock text-primary me-2"></i> <?= tgl_indo($article['tgl_upload']) ?>&nbsp;
-					<?php if ($article['kategori']) : ?>
+				<?php if ($article['kategori']) : ?>
 					<a href="<?= site_url('first/kategori/' . $article['kat_slug']) ?>">
 						<i class="fa fa-folder text-primary me-2"></i> <?= $article['kategori'] ?>
 					</a>
 					<?php endif ?>&nbsp;
 
-				<i class="fa fa-eye text-primary me-2"></i> <?= hit($article['hit']) ?>&nbsp;
+					<i class="fa fa-eye text-primary me-2"></i> <?= hit($article['hit']) ?>&nbsp;
 			</p>
 
 		</div>
-		<div class=" py-3 wow fadeInUp" data-wow-delay="0.7s">
+		<div class=" py-3 wow fadeInLeft" data-wow-delay="0.3s">
 			<?= $article['isi'] ?>
 			<?php for ($i = 1; $i <= 3; $i++) : ?>
 			<?php endfor ?>
@@ -37,11 +37,13 @@
 				</div>
 			<?php endif ?>
 		</div>
-		<div class="d-flex">
-		<?php $this->load->view($folder_themes . '/commons/share') ?>
+		<div class="d-flex pt-2 text-center wow fadeInRight" data-wow-delay="0.3s">
+			<a class="btn btn-outline-danger btn-social" href="" target="_blank"><i class="fab fa-twitter"></i></a>&nbsp;
+			<a class="btn btn-outline-danger btn-social" href="http://www.facebook.com/sharer.php?u=<?= site_url('artikel/' . buat_slug($article)) ?>" target="_blank"><i class="fab fa-facebook-f"></i></a>&nbsp;
+			<a class="btn btn-outline-danger btn-social" href="https://api.whatsapp.com/send?l=id&text=<?= site_url('artikel/' . buat_slug($article)) ?>" target="_blank"><i class="fab fa-whatsapp"></i></a>&nbsp;
+			<a class="btn btn-outline-danger btn-social" href="" target="_blank"><i class="fab fa-telegram"></i></a>&nbsp;
+			<a class="btn btn-outline-danger btn-social" href="" target="_blank"><i class="fab fa-instagram"></i></a>
 		</div>
-
-
 	</div>
 
 	<div class="entry-footer clearfix">
