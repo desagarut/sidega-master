8e9e172517a622f4ddf8750a2e89bbf5e24a9476
Desa Garut <desagarut.net@gmail.com>
2023-05-24 11:39:20 +0700

    Update sinergi.php

--- themes/bodas/partials/sinergi.php
+++ themes/bodas/partials/sinergi.php
@@ -15,7 +15,7 @@
             <?php foreach ($baris as $baris_program) : ?>
                 <?php $width = 100 / count($baris_program) - count($baris_program) ?>
                 <?php foreach ($baris_program as $key => $program) : ?>
-                    <a href="<?= $program['tautan'] ?>" target="_blank">">
+                    <a href="<?= $program['tautan'] ?>" target="_blank">
                         <div class="testimonial-item text-center">
                             <img class="border rounded-circle p-2 mx-auto mb-3" src="<?= base_url() . LOKASI_GAMBAR_WIDGET . $program['gambar'] ?>" style="width: 80px; height: 80px;">
                             <h5 class="mb-0"><?= $program['judul'] ?></h5>
