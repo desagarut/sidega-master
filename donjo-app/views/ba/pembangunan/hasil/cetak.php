3710d0b2c0c2b737233c725708422e86e5d09ccb
Desa Garut <desagarut.net@gmail.com>
2023-09-05 11:09:23 +0700

    Update cetak.php

--- donjo-app/views/ba/pembangunan/hasil/cetak.php
+++ donjo-app/views/ba/pembangunan/hasil/cetak.php
@@ -1,47 +1,4 @@
-<?php
-
-defined('BASEPATH') || exit('No direct script access allowed');
-
-/*
- * File ini:
- *
- * View untuk modul Buku Pembangunan Desa > Buku Rencana Pembangunan
- *
- * donjo-app/views/bumindes/pembangunan/rencana_kerja/cetak.php,
- */
-
-/*
- * File ini bagian dari:
- *
- * OpenSID
- *
- * Sistem informasi desa sumber terbuka untuk memajukan desa
- *
- * Aplikasi dan source code ini dirilis berdasarkan lisensi GPL V3
- *
- * Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
- *
- * Dengan ini diberikan izin, secara gratis, kepada siapa pun yang mendapatkan salinan
- * dari perangkat lunak ini dan file dokumentasi terkait ("Aplikasi Ini"), untuk diperlakukan
- * tanpa batasan, termasuk hak untuk menggunakan, menyalin, mengubah dan/atau mendistribusikan,
- * asal tunduk pada syarat berikut:
- *
- * Pemberitahuan hak cipta di atas dan pemberitahuan izin ini harus disertakan dalam
- * setiap salinan atau bagian penting Aplikasi Ini. Barang siapa yang menghapus atau menghilangkan
- * pemberitahuan ini melanggar ketentuan lisensi Aplikasi Ini.
- *
- * PERANGKAT LUNAK INI DISEDIAKAN "SEBAGAIMANA ADANYA", TANPA JAMINAN APA PUN, BAIK TERSURAT MAUPUN
- * TERSIRAT. PENULIS ATAU PEMEGANG HAK CIPTA SAMA SEKALI TIDAK BERTANGGUNG JAWAB ATAS KLAIM, KERUSAKAN ATAU
- * KEWAJIBAN APAPUN ATAS PENGGUNAAN ATAU LAINNYA TERKAIT APLIKASI INI.
- *
- * @copyright	  Hak Cipta 2009 - 2015 Combine Resource Institution (http://lumbungkomunitas.net/)
- * @copyright	  Hak Cipta 2016 - 2020 Perkumpulan Desa Digital Terbuka (https://opendesa.id)
- * @license	http://www.gnu.org/licenses/gpl.html	GPL V3
- *
- * @see 	https://github.com/OpenSID/OpenSID
- */
-?>
+<?php defined('BASEPATH') || exit('No direct script access allowed');?>
 
 <table>
 	<tbody>
@@ -93,10 +50,10 @@ defined('BASEPATH') || exit('No direct script access allowed');
 						<?php foreach ($main as $key => $data): ?>
 							<tr>
 								<td align="center"><?= ($key + 1)?></td>
-								<td><?= $data->judul; ?></td>
+								<td><?= $data->nama_program_kegiatan; ?></td>
 								<td align="center"><?= $data->volume; ?></td>
-								<td align="right"><?= Rupiah2($data->jml_anggaran); ?></td>
-								<td align="right"><?= $data->alamat; ?></td>
+								<td align="right"><?= Rupiah2($data->anggaran); ?></td>
+								<td align="right"><?= $data->lokasi; ?></td>
 								<td align="right"><?= $data->keterangan; ?></td>
 
 							</tr>
