a4ebb8f41c7e2fc1b7ecab157450bf9b4099ff82
Desa Garut <desagarut.net@gmail.com>
2024-02-23 14:39:52 +0700

    Update bagan.php

--- district-app/views/home/bagan.php
+++ district-app/views/home/bagan.php
@@ -1,12 +1,12 @@
-<link rel="stylesheet" href="<?= base_url()?>assets/css/bagan.css">
+<link rel="stylesheet" href="<?= base_url() ?>assets/css/bagan.css">
 
 <div class="content-wrapper">
 	<section class="content-header">
-		<h1>Bagan Pemerintahan <?= ucwords($this->setting->sebutan_desa)?></h1>
+		<h1>Bagan Pemerintahan <?= ucwords($this->setting->sebutan_desa) ?></h1>
 		<ol class="breadcrumb">
-			<li><a href="<?= site_url('beranda')?>"><i class="fa fa-home"></i> Home</a></li>
-			<li><a href="<?= site_url('pengurus')?>">Pemerintahan <?= ucwords($this->setting->sebutan_desa)?></a></li>
-			<li class="active">Bagan Pemerintahan <?= ucwords($this->setting->sebutan_desa)?></li>
+			<li><a href="<?= site_url('beranda') ?>"><i class="fa fa-home"></i> Home</a></li>
+			<li><a href="<?= site_url('pengurus') ?>">Pemerintahan <?= ucwords($this->setting->sebutan_desa) ?></a></li>
+			<li class="active">Bagan Pemerintahan <?= ucwords($this->setting->sebutan_desa) ?></li>
 		</ol>
 	</section>
 	<section class="content" id="maincontent">
@@ -15,9 +15,9 @@
 				<div class="box box-info">
 					<div class="box-body">
 						<figure class="highcharts-figure">
-					    <div id="container"></div>
-					    <p class="highcharts-description">
-					    </p>
+							<div id="container"></div>
+							<p class="highcharts-description">
+							</p>
 						</figure>
 					</div>
 				</div>
