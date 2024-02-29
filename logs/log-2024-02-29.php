d9d76d0fe9740c030f67064985487e911370d60f
Desa Garut <desagarut.net@gmail.com>
2024-02-29 13:45:32 +0700

    Create log-2024-02-29.php

---
+++ logs/log-2024-02-29.php
@@ -0,0 +1,47 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-02-29 10:11:46 --> Curl error: SSL certificate problem: unable to get local issuer certificate
+ERROR - 2024-02-29 10:11:46 --> Array
+(
+    [url] => https://trace.desagarut.id/index.php/api/track/desa?token=da39a3ee5e6b4b0d3255bfef95601890afd80709
+    [content_type] => 
+    [http_code] => 0
+    [header_size] => 0
+    [request_size] => 0
+    [filetime] => -1
+    [ssl_verify_result] => 20
+    [redirect_count] => 0
+    [total_time] => 0.051955
+    [namelookup_time] => 0.012717
+    [connect_time] => 0.022746
+    [pretransfer_time] => 0
+    [size_upload] => 0
+    [size_download] => 0
+    [speed_download] => 0
+    [speed_upload] => 0
+    [download_content_length] => -1
+    [upload_content_length] => -1
+    [starttransfer_time] => 0
+    [redirect_time] => 0
+    [redirect_url] => 
+    [primary_ip] => 203.175.9.48
+    [certinfo] => Array
+        (
+        )
+
+    [primary_port] => 443
+    [local_ip] => 192.168.1.103
+    [local_port] => 61781
+    [http_version] => 0
+    [protocol] => 2
+    [ssl_verifyresult] => 0
+    [scheme] => HTTPS
+    [appconnect_time_us] => 0
+    [connect_time_us] => 22746
+    [namelookup_time_us] => 12717
+    [pretransfer_time_us] => 0
+    [redirect_time_us] => 0
+    [starttransfer_time_us] => 0
+    [total_time_us] => 51955
+)
+
