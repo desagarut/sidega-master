0d07c7550bbf2a31453e7f88b0562b932d31c1fb
Desa Garut <desagarut.net@gmail.com>
2025-05-12 00:15:15 +0700

    Update template.php

--- themes/bodas/template.php
+++ themes/bodas/template.php
@@ -3,7 +3,7 @@
 <?php $this->load->view($folder_themes . '/commons/head') ?>
 
 <body>
-    <?php $this->load->view($folder_themes . '/commons/spinner.php') ?>
+    <?php // $this->load->view($folder_themes . '/commons/spinner.php') ?>
     <?php $this->load->view($folder_themes . '/commons/nav.php') ?>
     <?php $this->load->view($folder_themes . '/layouts/home.tpl.php') ?>
     <?php $this->load->view($folder_themes . '/commons/footer') ?>
