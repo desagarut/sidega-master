390cab9cc2d2d2669b769ab1e6bb85e38fbe191f
Desa Garut <desagarut.net@gmail.com>
2025-05-15 13:48:34 +0700

    Update Slider
    Add Foto Bupati

--- themes/bodas/partials/carousel.php
+++ themes/bodas/partials/carousel.php
@@ -6,26 +6,37 @@
             <img class="img-fluid" style="height: 650px;" src="<?= gambar_desa($desa['kantor_desa']) ?>" alt="<?= ucfirst($this->setting->sebutan_desa) . ' ' . ucwords($desa['nama_desa']) ?>">
             <div class="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style="background: rgba(2, 117, 34, 0.7);">
                 <div class="container">
-                    <div class="row justify-content-start">
-                        <div class="col-md-8">
-                            <h1 class="display-7 text-white animated slideInDown">
-                                Selamat datang di <br>
-                                <?= ucfirst($this->setting->sebutan_desa) . ' ' . ucwords($desa['nama_desa']) ?> <br />
-                                <?= ucfirst($this->setting->sebutan_kecamatan) . ' ' . ucwords($desa['nama_kecamatan']) ?><br />
-                                <?= ucfirst($this->setting->sebutan_kabupaten) . ' ' . ucwords($desa['nama_kabupaten']) ?><br />
-                                Provinsi Jawa Barat
+                    <div class="row justify-content-center">
+                        <div class="col-md-4 d-none d-sm-block">
+                            <div class="team-item animated slideInLeft" style="padding:10px 15px 0px 15px">
+                                <div class="overflow-hidden  mb-2">
+                                    <img class="img-fluid" style="object-fit: content; height:300px; " src="<?= base_url() ?>assets/files/user_pict/Bupati-Garut.png" alt="Bupati dan Wakil Bupati Garut">
+                                </div>
+                                <div class="text-center bg-light">
+                                    <p class="mb-0">Bupati & Wakil Bupati Garut</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-5 mt-10 text-center team-item animated slideInDown">
+                            <h1>
+                                <small class="text-white"> Selamat datang di </small><br>
+                                <span class="display-7 text-white"><?= strtoupper($this->setting->sebutan_desa) . ' ' . strtoupper($desa['nama_desa']) ?> </span><br />
+                                <small class="display-10 text-white"><?= ucfirst($this->setting->sebutan_kecamatan) . ' ' . ucwords($desa['nama_kecamatan']) ?></small><br />
+                                <small class="display-10 text-white"><?= ucfirst($this->setting->sebutan_kabupaten) . ' ' . ucwords($desa['nama_kabupaten']) ?></small><br />
+                                <small class="display-10 text-white">Provinsi Jawa Barat</small>
                             </h1>
                             <a href="<?= site_url('arsip') ?>" target="_blank" class="btn btn-info py-md-3 px-md-5 me-3 animated slideInLeft">Berita</a>
                             <a href="<?= site_url('first/wilayah') ?>" class="btn btn-light py-md-3 px-md-5 animated slideInRight">Statistik</a>
                         </div>
-                        <div class="col-md-4">
-                            <div class="team-item shadow animated slideInRight" style="padding:10px 15px 0px 15px">
+                        <div class="col-md-3 d-none d-sm-block">
+                            <div class="team-item animated slideInRight" style="padding:10px 15px 0px 15px">
                                 <div class="overflow-hidden">
-                                    <img class="img-fluid" style="object-fit: cover; height:350px; " src="<?= base_url() ?>instansi/upload/user_pict/<?= $pamong_kades['foto'] ?>" alt="<?= $pamong_kades['pamong_nama'] ?>">
+                                    <img class="img-fluid" style="object-fit: cover; height:300px; " src="<?= base_url() ?>instansi/upload/user_pict/<?= $pamong_kades['foto'] ?>" alt="<?= $pamong_kades['pamong_nama'] ?>">
                                 </div>
-                                <div class="text-center bg-light p-4">
-                                    <h4 class="mb-0"><?= $pamong_kades['pamong_nama'] ?></h4>
-                                    <h6><?= strtoupper($pamong_kades['jabatan']) . ' ' . strtoupper($desa['nama_desa']) ?></h6>
+                                <div class="text-center bg-light">
+                                    <p class="mb-0"><b><?= $pamong_kades['pamong_nama'] ?></b><br>
+                                        <small><?= strtoupper($pamong_kades['jabatan']) . ' ' . strtoupper($desa['nama_desa']) ?></small>
+                                    </p>
                                 </div>
                             </div>
                         </div>
