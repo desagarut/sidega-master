08a0ca0d8d8b59698bff8c4d261127c4cba58669
Desa Garut <desagarut.net@gmail.com>
2025-05-20 20:36:53 +0700

    Update nav.php

--- themes/bodas/commons/nav.php
+++ themes/bodas/commons/nav.php
@@ -17,7 +17,7 @@
     <div class="collapse navbar-collapse" id="navbarCollapse">
         <div class="navbar-nav ms-auto p-4 p-lg-0">
             <a href="<?= site_url('first') ?>" class="nav-item nav-link active">Home</a>
-            <?php if (menu_atas) : ?>
+            <?php //if (menu_atas) : ?>
                 <?php foreach ($menu_atas as $menu) : ?>
 
                     <div class="nav-item dropdown">
@@ -33,7 +33,7 @@
 
                     </div>
                 <?php endforeach ?>
-            <?php endif ?>
+            <?php // endif ?>
         </div>
         <a href="<?= site_url('mandiri_web') ?>" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">LOGIN<i class="fa fa-arrow-right ms-3"></i></a>
     </div>
