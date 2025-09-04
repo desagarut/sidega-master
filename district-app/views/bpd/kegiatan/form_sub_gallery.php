a02ea810b528851e6e1dc085f162c597a9fff935
Desa Garut <desagarut.net@gmail.com>
2025-09-04 10:37:23 +0700

    Update Kegiatan BPD
    Perbaikan form sub gallery, sub gallery table

--- district-app/views/bpd/kegiatan/form_sub_gallery.php
+++ district-app/views/bpd/kegiatan/form_sub_gallery.php
@@ -1,11 +1,11 @@
 <div class="content-wrapper">
 	<section class="content-header">
-		<h1>Pengaturan Gambar Album</h1>
+		<h1>Administrasi BPD - Form Dokumentasi Kegiatan</h1>
 		<ol class="breadcrumb">
 			<li><a href="<?= site_url('beranda')?>"><i class="fa fa-home"></i> Home</a></li>
-			<li><a href="<?= site_url('gallery')?>"><i class="fa fa-dashboard"></i> Daftar Album</a></li>
-			<li><a href='<?= site_url("gallery/sub_gallery/$album")?>'><i class="fa fa-dashboard"></i> Daftar Gambar Album</a></li>
-			<li class="active">Pengaturan Gambar Album</li>
+			<li><a href="<?= site_url('bpd_buku_kegiatan')?>"><i class="fa fa-dashboard"></i> Daftar Kegiatan</a></li>
+			<li><a href='<?= site_url("bpd_buku_kegiatan/sub_gallery/$album")?>'><i class="fa fa-dashboard"></i> Dokumentasi</a></li>
+			<li class="active">Form Dokumentasi Kegiatan</li>
 		</ol>
 	</section>
 	<section class="content" id="maincontent">
@@ -14,23 +14,23 @@
 				<div class="col-md-12">
 					<div class="box box-info">
             <div class="box-header with-border">
-							<a href="<?= site_url("gallery/sub_gallery/$album")?>" class="btn btn-social btn-box btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"  title="Tambah Artikel">
-								<i class="fa fa-arrow-circle-left "></i>Kembali ke Daftar Gambar Album
+							<a href="<?= site_url("bpd_buku_kegiatan/sub_gallery/$album")?>" class="btn btn-social btn-box btn-info btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"  title="Tambah Artikel">
+								<i class="fa fa-arrow-circle-left "></i>Kembali ke Daftar Kegiatan
             	</a>
 						</div>
 						<div class="box-body">
 							<div class="form-group">
 								<label class="control-label col-sm-4" for="nama">Nama Gambar</label>
 								<div class="col-sm-6">
-									<input name="nama" class="form-control input-sm nomor_sk" maxlength="50" type="text" value="<?=$gallery['nama']?>"></input>
+									<input name="nama" class="form-control input-sm nomor_sk" maxlength="50" type="text" value="<?=$bpd_buku_kegiatan['nama']?>"></input>
 								</div>
 							</div>
-							<?php if ($gallery['gambar']): ?>
+							<?php if ($bpd_buku_kegiatan['gambar']): ?>
 								<div class="form-group">
 									<label class="control-label col-sm-4" for="nama"></label>
 									<div class="col-sm-6">
-										<input type="hidden" name="old_gambar" value="<?=  $gallery['gambar']?>">
-									  <img class="attachment-img img-responsive img-circle" src="<?= AmbilGaleri($gallery['gambar'], 'sedang') ?>" alt="Gambar Album">
+										<input type="hidden" name="old_gambar" value="<?=  $bpd_buku_kegiatan['gambar']?>">
+									  <img class="attachment-img img-responsive img-circle" src="<?= AmbilGaleri($bpd_buku_kegiatan['gambar'], 'sedang') ?>" alt="Gambar Album">
 									</div>
 								</div>
 							<?php endif; ?>
@@ -38,7 +38,7 @@
 									<label class="control-label col-sm-4" for="upload">Unggah Gambar</label>
 									<div class="col-sm-6">
 										<div class="input-group input-group-sm">
-											<input type="text" class="form-control <?php !($gallery['gambar']) and print('required') ?>" id="file_path">
+											<input type="text" class="form-control <?php !($bpd_buku_kegiatan['gambar']) and print('required') ?>" id="file_path">
 											<input id="file" type="file" class="hidden" name="gambar">
 											<span class="input-group-btn">
 												<button type="button" class="btn btn-info btn-box"  id="file_browser"><i class="fa fa-search"></i> Browse</button>
