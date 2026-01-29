1b4522e760486ed8167db91c10ae0fb12f0c0b8e
Desa Garut <desagarut.net@gmail.com>
2026-01-29 08:35:24 +0700

    Upadte v 6.3.1
    - Perbaikan tata letak foto anggota BPD

--- themes/bodas/partials/profil/bpd.php
+++ themes/bodas/partials/profil/bpd.php
@@ -1,64 +1,58 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 
 <!-- Service Start -->
-<div class="row justify-content-center">
-	<h3 class="mb-5 text-center">BADAN PERMUSYAWARATAN DESA </h3>
+<div class="container">
+	<div class="row justify-content-center">
+		<h3 class="p-2 text-center">BADAN PERMUSYAWARATAN DESA </h3>
 
 
-	<?php foreach ($bpd as $data) : ?>
-		<div class="course-item p-4">
-			<div class="row g-4 p-4 shadow justify-content-center">
-				<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-					<div class="course-item bg-info">
-						<div class="position-relative overflow-hidden text-center" style="padding: 5px 5px 5px 5px; height: 100%">
-							<?php if ($data['foto']): ?>
-								<img class="img-fluid" src="<?= AmbilFoto($data['foto']) ?>" alt="<?= $data['nama'] ?>" style="object-fit: cover; height:600px; width:auto" />
-							<?php else: ?>
-								<img class="img-fluid" src="<?= base_url() ?>assets/files/user_pict/kuser.png" alt="<?= $data['nama'] ?>" sstyle="object-fit: cover; height:600px; width:auto" />
-							<?php endif ?>
+		<?php foreach ($bpd as $data) : ?>
+			<div class="course-item p-4 wow fadeInUp" data-wow-delay="0.1s">
+				<div class="row g-2 p-4 shadow justify-content-center">
+					<div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+						<div class="course-item">
+							<div class="position-relative overflow-hidden text-center" style="padding: 10px 10px 10px 10px; height: 100%">
+								<?php if ($data['foto']): ?>
+									<img class="img-fluid" src="<?= AmbilFoto($data['foto']) ?>" alt="foto <?= $data['nama'] ?>" style="object-fit: cover; height:400px; width:auto" />
+								<?php else: ?>
+									<img class="img-fluid" src="<?= base_url() ?>assets/files/user_pict/kuser.png" alt="<?= $data['nama'] ?>" sstyle="object-fit: cover; height:600px; width:auto" />
+								<?php endif ?>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div class="col-md-8 wow fadeInUp" data-wow-delay="0.1s">
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="table-responsive">
-								<table class="table table-responsive table-borderless">
-									<thead>
-										<tr>
-											<th colspan="3">BIODATA:</th>
-									</thead>
-									<tbody>
-										<tr>
-											<td style="width: 20%;">NAMA</td>
+					<div class="col-lg-8 wow fadeInUp" data-wow-delay="0.1s">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="table-responsive">
+									<table class="table table-responsive table-borderless">
+										<thead>
+											<tr>
+												<th colspan="3"><h5> POFILE BPD</h5></th>
+										</thead>
+										<tbody>
+											<tr>
+												<td style="width: 20%;">NAMA</td>
+												<td style="width: 2%;">:</td>
+												<td class="width: 78%"><?= strtoupper($data['nama']) ?></td>
+											<tr>
+											</tr>
+											<td style="width: 20%;">JABATAN</td>
 											<td style="width: 2%;">:</td>
-											<td class="width: 78%"><?= strtoupper($data['nama']) ?></td>
-										<tr>
-										</tr>
-										<td style="width: 20%;">JABATAN</td>
-										<td style="width: 2%;">:</td>
-										<td class="width: 78%"><?= strtoupper($data['jabatan']) ?></td>
-										</tr>
-									</tbody>
-								</table>
-								<table class="table table-responsive table-borderless">
-									<thead>
-										<tr>
-											<th colspan="3">TUGAS POKOK DAN FUNGSI:</th>
-									</thead>
-									<tbody>
-										<tr>
-											<td>
-												<div class="card card-body"><?= $data['tupoksi'] ?></div>
-											</td>
-										</tr>
-									</tbody>
-								</table>
+											<td class="width: 78%"><?= strtoupper($data['jabatan']) ?></td>
+											</tr>
+											</tr>
+											<td style="width: 20%;">TUPOKSI</td>
+											<td style="width: 2%;">:</td>
+											<td class="width: 78%"><?= strtoupper($data['tupoksi']) ?></td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	<?php endforeach; ?>
+		<?php endforeach; ?>
+	</div>
 </div>
